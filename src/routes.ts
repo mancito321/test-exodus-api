ea6150ad779db999ac86dc8585ceca1cf493479f
Juan Llano <juan.llano@gorillalogic.com>
2024-09-20 17:04:11 -0500

    Add basic elemetns

---
+++ src/routes.ts
@@ -0,0 +1,32 @@
+import Router from "koa-router";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+const API_KEY = process.env.AUTH_CGPT;
+const ORG_KEY = process.env.AUTH_CGPT;
+const PRJ_KEY = process.env.AUTH_CGPT;
+
+const openai = new OpenAI({
+  organization: ORG_KEY,
+  project: PRJ_KEY,
+  apiKey: API_KEY,
+});
+
+export const router = new Router();
+
+router.get("/product", async (ctx, next) => {
+  try {
+    //Call Mongodb database
+    //Get Data from db
+    //Call GPT-3 API
+    // const completion = await openai.chat.completions.create({
+    //   messages: [{ role: "user", content: "Tell me a debug joke" }],
+    //   model: "gpt-3.5-turbo",
+    // });
+    // console.log(completion.choices[0]);
+  } catch (error) {
+    console.error("Error communicating with OpenAI API:", error);
+  }
+  ctx.body = "Hello World! from API";
+});
